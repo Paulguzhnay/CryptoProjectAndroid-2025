aa93ddb0a0765236d0165a703b2578197f994f17
Paulguzhnay <paul-guzhnay@hotmail.com>
2025-04-05 14:29:52 -0500

    Creacion del menu principal con espacios

--- app/src/main/java/com/example/cryptocapapp/views/AssetMainMenu.kt
+++ app/src/main/java/com/example/cryptocapapp/views/AssetMainMenu.kt
@@ -4,18 +4,24 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.FlowRowScopeInstance.align
+
 
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 
@@ -26,7 +32,7 @@ fun AssetRow(textAR:String, size: Int){
     ){
         Text(
             text=textAR,
-            fontSize = size.sp
+            fontSize = size.sp,
             )
         }
 
@@ -56,6 +62,7 @@ fun AssetRowPreview(){
             .background(
                 color = Color.Cyan
             )
+
 
     ){
         AssetRow("Crypto Menu",30)
